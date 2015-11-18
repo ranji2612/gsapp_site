e7c67a95870ec19a5d55564fed44bf231e4a62c7
ranji2612 <ranji2612@gmail.com>
2015-11-18 16:47:23 -0500

    minor changes

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -3,7 +3,7 @@ module.exports = function(app, passport) {
 	// Load the home page for others apart from api
     app.get('/*', function(req, res){
 		
-		res.sendfile('public/html/home.html');	
+		res.sendfile('public/index.html');	
 		
 	});
 	
